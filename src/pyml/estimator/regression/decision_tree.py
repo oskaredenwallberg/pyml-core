79aa9b73bc39d3f3d3f563285eb79ad35b087f6e
oskarew <oswa.public@gmail.com>
2026-08-22 15:56:10 +0200

    decision tree regression fit method optimized

--- src/pyml/estimator/regression/decision_tree.py
+++ src/pyml/estimator/regression/decision_tree.py
@@ -14,8 +14,6 @@ def variance_reduction(y: NDArray, y1: NDArray, y2: NDArray):
     return score
 
 
-
-
 class DecisionTree(Estimator, Tree):
     def __init__(
             self,
@@ -63,28 +61,38 @@ class DecisionTree(Estimator, Tree):
         if self.skip(node) == True:
             return j_star, t_star
 
+        yi = y[node.index]
+        xi = x[node.index]
+        n = yi.size
+
         for j in range(F):
-            xij = x[node.index, j]
-            yi  = y[node.index]
-            num_dt = min(node.num_samples, 10)
-            x05 = np.quantile(xij, q=0.05)
-            x95 = np.quantile(xij, q=0.95)
-            dt = (x95 - x05) / num_dt
+            xij = xi[:, j]
+            order = np.argsort(xij)
+            x_sorted = xij[order]
+            valid = x_sorted[1:] != x_sorted[:-1]
+            valid[:self.min_samples-1] = False
+            valid[n-self.min_samples:] = False
 
-            for k in range(1, num_dt):
-                t = x05 + k*dt
-                mask = xij < t
+            k = np.flatnonzero(valid)
+            if k.size == 0:
+                continue
 
-                y1, y2 = yi[mask], yi[~mask]
-                if y1.size < self.min_samples or y2.size < self.min_samples:
-                    continue
-                
-                score = self.criterion(yi, y1, y2)
+            lsum = np.cumsum(yi[order])
+            lssq = np.cumsum(yi[order] ** 2)
+            rsum = lsum[-1] - lsum
+            rssq = lssq[-1] - lssq
 
-                if score > best:
-                    best = score
-                    t_star = t
-                    j_star = j
+            sse  = lssq[-1] - lsum[-1] ** 2 / n
+            sse1 = lssq[k] - lsum[k] ** 2 / (k+1)
+            sse2 = rssq[k] - rsum[k] ** 2 / (n-k-1)
+
+            scores = sse - sse1 - sse2
+            argmax = np.argmax(scores)
+
+            if scores[argmax] > best:
+                best = scores[argmax]
+                j_star = j
+                t_star = (x_sorted[k[argmax]] + x_sorted[k[argmax]+1]) / 2
 
         return j_star, t_star
 
@@ -108,10 +116,7 @@ class DecisionTree(Estimator, Tree):
         return predictions
 
 
-
-# class DecisionTree2(DecisionTree):
-#     def fit():
-#         pass
+# score = self.criterion(yi, y1, y2)
 
 
 
@@ -124,5 +129,56 @@ class DecisionTree(Estimator, Tree):
 # 3. Find the feature and threshold improving the criterion the most
 # 4. Create child nodes, split index
 
+
+
+# def search_split(self, x: NDArray, y: NDArray, node: Node):
+#     N, F = x.shape
+#     best = 0.0
+#     t_star = None
+#     j_star = None
+
+#     if self.skip(node):
+#         return j_star, t_star
+
+#     yi = y[node.index]
+#     xi = x[node.index]
+#     n = yi.size
+#     totsum = np.sum(yi)
+#     totssq = np.sum(yi ** 2)
+#     totsse = totssq - totsum**2 / n
+
+#     for j in range(F):
+#         xij = xi[:, j]
+#         order = np.argsort(xij)
+
+#         x_sorted = xij[order]
+#         y_sorted = yi[order]
+
+#         lsum, rsum = 0.0, totsum
+#         lssq, rssq = 0.0, totssq
+
+#         for k in range(n-1):
+#             yk = y_sorted[k]
+#             lsum, rsum = lsum+yk,    rsum-yk
+#             lssq, rssq = lssq+yk**2, rssq-yk**2
+
+#             if x_sorted[k] == x_sorted[k + 1]:
+#                 continue
+
+#             n1 = k + 1
+#             n2 = n - n1
+#             if (n1 < self.min_samples) or (n2 < self.min_samples):
+#                 continue
+
+#             lsse = lssq - lsum**2 / n1
+#             rsse = rssq - rsum**2 / n2
+#             score = totsse - lsse - rsse
+
+#             if score > best:
+#                 best = score
+#                 j_star = j
+#                 t_star = (x_sorted[k] + x_sorted[k + 1]) / 2
+
+#     return j_star, t_star
 
     
